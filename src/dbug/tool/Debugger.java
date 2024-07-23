bdbc1694be4f520556a410e71cdfaf682fe97513
JP16D <jppatarata16@gmail.com>
2024-07-23 21:51:59 +0800

    update Debugger.java

--- src/dbug/tool/Debugger.java
+++ src/dbug/tool/Debugger.java
@@ -96,8 +96,8 @@ public class Debugger extends Table {
 		float lw = 0f;
 		float vw = 0f;
 		for (var main : getCells()) {
-			var label = main.getCells().get(0);
-			var value = main.getCells().get(1);
+			var label = main.get().getCells().get(0);
+			var value = main.get().getCells().get(1);
 			//
 			lw = Math.max(lw, label.get().getWidth());
 			vw = Math.max(vw, value.get().getWidth());
