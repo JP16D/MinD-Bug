4c894172a19fe9ef4af9f86bf69741bb4c4901b9
JP16D <jppatarata16@gmail.com>
2024-07-23 22:41:35 +0800

    update Debugger.java

--- src/dbug/tool/Debugger.java
+++ src/dbug/tool/Debugger.java
@@ -64,8 +64,8 @@ public class Debugger extends Table {
 			var v = map.get(k);
 			//
 			var main = table(Tex.whiteui).left().pad(4f).get();
-			var label = main.table().center().pad(4f).get();
-			var value = main.table(Tex.whiteui).pad(2f).color(Color.black).get();
+			var label = main.table().center().pad(2f, 4f, 2f, 4f).get();
+			var value = main.table(Tex.whiteui).pad(4f).color(Color.black).get();
 			//
 			if (v.get() instanceof Debuggable d) {
 				//
@@ -81,7 +81,7 @@ public class Debugger extends Table {
 					//
 					map.put(k, () -> new Debuggable(d.type, txt));
 					//
-				}).center().pad(2f);
+				}).center();
 				//
 			} else {
 				//
@@ -89,19 +89,23 @@ public class Debugger extends Table {
 				value.add("" + v.get()).pad(8f);
 			}
 			//
-			label.add(new Label(k, Styles.outlineLabel)).center().pad(4f, 8f, 4f, 8f).get().setColor(main.color);
+			label.add(new Label(k, Styles.outlineLabel)).center().pad(2f, 8f, 2f, 8f).get().setColor(main.color);
 			row();
 		}
 		//
+		float mw = 0f;
 		float lw = 0f;
 		float vw = 0f;
 		for (var main : getCells()) {
+			mw = Math.max(mw, main.get().getWidth());
 			//
 			lw = Math.max(lw, ((Table) main.get()).getCells().get(0).get().getWidth());
 			vw = Math.max(vw, ((Table) main.get()).getCells().get(1).get().getWidth());
 		}
 		//
 		for (var main : getCells()) {
+			main.get().setWidth(mw);
+			//
 			((Table) main.get()).getCells().get(0).get().setWidth(lw);
 			((Table) main.get()).getCells().get(1).get().setWidth(vw);
 		}
