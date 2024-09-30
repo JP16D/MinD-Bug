d024c21c9fa8143685fcdbd0dff8c71af0cd53e5
JP16D <jppatarata16@gmail.com>
2024-09-30 08:29:53 +0800

    update MDBugVars.java

--- src/dbug/MDBugVars.java
+++ src/dbug/MDBugVars.java
@@ -4,7 +4,7 @@ import arc.*;
 import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
 import arc.struct.*;
-import dbug.tools.*;
+import dbug.tool.*;
 
 public class MDBugVars {
 	public static final OrderedMap<String, Table> debugger = new OrderedMap<>();
