25aa0af3050ebe8ff6e8e00193c0409d2cd245e6
JP16D <jppatarata16@gmail.com>
2024-10-28 15:56:22 +0800

    update Debuggable.java

--- src/dbug/tool/Debuggable.java
+++ src/dbug/tool/Debuggable.java
@@ -70,12 +70,15 @@ public class Debuggable {
 		} else {
 			return Debugger.table(Color.maroon, name, new Table(t -> {
 				//
+				boolean archived = false;
+				//
 				for (var k : fields.keys()) {
 					t.add(Debugger.display(Color.darkGray, k.getName(), new Table(ft -> {
 						ft.field((String) dv(k.getName() + "txt-init", () -> fields.get(k).get().toString()).get(), Styles.defaultField, (String txt) -> {
 							//
 							fields.put(k, parse(wrap(k.getType()), fields.get(k), txt).v2);
 							//
+							archived = true;
 							dv(k.getName() + "txt", () -> txt);
 						}).center().pad(4f);
 						dv(k.getName() + "txt-post", () -> fields.get(k).get().toString());
@@ -94,8 +97,9 @@ public class Debuggable {
 					}
 				}).right().pad(2f);
 				//
-				t.button(Icon.cancel, () -> {
+				if (archived) t.button(Icon.cancel, () -> {
 					set(this.type, this.value);
+					archived = false;
 				}).right().pad(2f);
 				//
 			}));
