0714e81f4df177c39979edbbd756ae08f5e03922
JP16D <jppatarata16@gmail.com>
2024-10-30 00:05:09 +0800

    update Debuggable.java

--- src/dbug/tool/Debuggable.java
+++ src/dbug/tool/Debuggable.java
@@ -73,7 +73,7 @@ public class Debuggable {
 					set(parse(type, value, txt));
 					//
 					priority = true;
-					Debugger.dv("prio", priority);
+					Debugger.dv(name + "-prio", priority);
 				}).center().pad(4f);
 			}));
 			//
