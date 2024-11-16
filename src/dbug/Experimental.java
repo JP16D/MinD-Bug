2e9a221bb830d64856ab22e7edbd81dbd75543f7
JP16D <jppatarata16@gmail.com>
2024-11-16 21:22:43 +0800

    update

--- src/dbug/Experimental.java
+++ src/dbug/Experimental.java
@@ -8,11 +8,9 @@ public class Experimental {
 	public Vec2 test = new Vec2();
 	
 	public void loadTest() {
-		test.x = (float) dw(Float.class, "pre-local-x", test.x);
-		test.y = (float) dw(Float.class, "pre-local-y", test.y);
+		test.x = (float) dw(Float.class, "local-x", test.x);
+		test.y = (float) dw(Float.class, "local-y", test.y);
 		test = (Vec2) dw(Vec2.class, "localTest", test);
-		test.x = (float) dw(Float.class, "post-local-x", test.x);
-		test.y = (float) dw(Float.class, "post-local-y", test.y);
 		dv("test-vx", test.x);
 		dv("test-vy", test.y);
 	}
--- src/dbug/tool/Debugger.java
+++ src/dbug/tool/Debugger.java
@@ -37,8 +37,6 @@ public class Debugger {
 		//
 		var v = writable.get(name);
 		//
-		v.set(val);
-		//
 		debugger.put(name, v.table(name));
 		//
 		return v.value;
