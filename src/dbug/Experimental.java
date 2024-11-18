089aef3df2030272014412dea73bbc40f33ef9df
JP16D <jppatarata16@gmail.com>
2024-11-18 23:12:49 +0800

    update Experimental.java

--- src/dbug/Experimental.java
+++ src/dbug/Experimental.java
@@ -8,8 +8,8 @@ public class Experimental {
 	public Vec2 test = new Vec2();
 	
 	public void loadTest() {
-		test.x = (float) dw(int.class, "local-x", test.x);
-		test.y = (float) dw(int.class, "local-x", test.y);
+		test.x = (float) dw(float.class, "local-x", test.x);
+		test.y = (float) dw(float.class, "local-y", test.y);
 		test = (Vec2) dw(Vec2.class, "localTest", test);
 		dv("test-vx", test.x);
 		dv("test-vy", test.y);
