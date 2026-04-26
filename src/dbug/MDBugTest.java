6d7f265ece9aed37ef35ee5405c108a1924e626c
JP16D <jppatarata16@gmail.com>
2026-04-26 18:51:02 +0800

    ...

---
+++ src/dbug/MDBugTest.java
@@ -0,0 +1,24 @@
+package dbug;
+
+import arc.math.geom.*;
+import arc.scene.style.*;
+import mindustry.gen.*;
+
+import static dbug.tool.Debugger.*;
+
+public class MDBugTest {
+	public static Vec2 test = new Vec2();
+	
+	public static void load() {
+		dv("test-x", test.x);
+		dv("test-y", test.y);
+		dv("test", test);
+		test.x = (float) dw("test-vx", test.x);
+		test.y = (float) dw("test-vy", test.y);
+		test = (Vec2) dw("test-v", test);
+		
+		for (var f : Icon.class.getFields()) try {
+			if (f.get(new Icon()) instanceof Drawable d) dv("icon-" + f.getName(), d);
+		} catch (Exception e) {}
+	}
+}
